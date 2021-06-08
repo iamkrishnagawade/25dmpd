638727a31135f1852380d3b464f04bfc060f6eea
Krishna Gawade <krishnag@synerzipune.local>
2021-06-08 23:39:25 +0530

    new package install: concurrently

--- server/api/config/db.config.js
+++ server/api/config/db.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config({ path: '../../.env' });
 
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASS,
-    DB: process.env.DB
+    HOST: process.env.DB_HOST || 'localhost',
+    USER: process.env.DB_USER || 'root',
+    PASSWORD: process.env.DB_PASS || '',
+    DB: process.env.DB || 'db_25dmpd'
 };
